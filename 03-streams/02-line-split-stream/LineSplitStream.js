2b1e9d6f0c743bb6b86d5558b98d997207a3f331
Pavel Korolev <p.korolev@.lamoda.ru>
2020-08-07 22:15:58 +0300

    03-streams/02-line-split-stream review fix

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -5,17 +5,50 @@ class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
     this.encoding = options.encoding;
-    this.data = '';
+
+    // кеш для chunk без EOL в виде строки
+    this.cache = '';
   }
 
   _transform(chunk, encoding, callback) {
-    this.data = this.data + chunk.toString(this.encoding);
+    // собираем строку из прошлых chunk без EOL и текущего chunk
+    const str = `${this.cache}${chunk.toString(this.encoding)}`;
+
+    if (!str.includes(EOL)) {
+      // если в полученной строке нет EOL, то обновляем кеш
+      this.cache = `${str}`;
+    } else {
+      // если есть EOL, то разбиваем строку на массив
+      const lines = str.split(EOL);
+
+      lines.forEach((line, index) => {
+        if (line) {
+          if (index === lines.length - 1) {
+            // если осталась последняя непустая line,
+            // значит у строки остался "хвост" — кешируем его
+            this.cache = `${line}`;
+          } else {
+            // все line, которые полностью разбились, отдаем в буфер стрима
+            this.push(line);
+          }
+        } else {
+          // если будет пустая line, значит строка оканчивалась на EOL и
+          // была целиком разбита — очищаем кеш.
+          this.cache = '';
+        }
+      });
+    }
+
     callback();
   }
 
   _flush(callback) {
-    this.data.split(EOL).forEach(str => this.push(str));
-    this.data = '';
+    if (this.cache) {
+      // если что-то осталось в кеше, то отдаем в буфер как последнюю line
+      // и очищаем кеш
+      this.push(this.cache);
+      this.cache = '';
+    }
     callback();
   }
 }
