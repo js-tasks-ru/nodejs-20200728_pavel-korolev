15d90415f5571cc0dcfd1fcc799826d06da31fa0
Pavel Korolev <p.korolev@.lamoda.ru>
2020-08-16 00:32:41 +0300

    06-koajs/01-chat-app solved

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -1,17 +1,32 @@
 const path = require('path');
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
 
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
-const Router = require('koa-router');
-const router = new Router();
+let subscriptions = [];
 
 router.get('/subscribe', async (ctx, next) => {
+  const getMessage = () => new Promise((resolve) => {
+    subscriptions.push(resolve);
+  });
+  const message = await getMessage();
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const { message } = ctx.request.body;
+
+  if (message) {
+    subscriptions.forEach(resolve => resolve(message));
+    subscriptions.length = 0;
+  }
+
+  ctx.body = 'OK';
 });
 
 app.use(router.routes());
