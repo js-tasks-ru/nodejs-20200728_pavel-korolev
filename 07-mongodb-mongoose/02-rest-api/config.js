7ab0e49015fc9b0d72db45881415df21ee2eb4d6
Pavel Korolev <p.korolev@.lamoda.ru>
2020-08-23 00:30:42 +0300

    07-mongodb-mongoose/02-rest-api solved

--- 07-mongodb-mongoose/02-rest-api/config.js
+++ 07-mongodb-mongoose/02-rest-api/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mongodb: {
     uri: (process.env.NODE_ENV === 'test') ?
-      'mongodb://localhost/6-module-2-task' :
+      'mongodb://localhost:27017' :
       'mongodb://localhost/any-shop',
   },
 };
