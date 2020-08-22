c6b08d46c0e9269c31b864153f3eeead2f487e0b
Pavel Korolev <p.korolev@.lamoda.ru>
2020-08-23 00:31:02 +0300

    07-mongodb-mongoose/03-search solved

--- 07-mongodb-mongoose/03-search/controllers/products.js
+++ 07-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,32 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+
+const mapProduct = product => ({
+  id: product._id,
+  title: product.title,
+  description: product.description,
+  price: product.price,
+  category: product.category,
+  subcategory: product.subcategory,
+  images: product.images,
+});
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const query = ctx.query.query;
+
+  if (
+    typeof query === 'string'
+    && !query
+    && !query.trim()
+  ) {
+    ctx.body = { products: [] };
+  }
+
+  const products = await Product.find({ $text: { $search: query } })
+
+  if (!products) {
+    ctx.body = { products: [] };
+  } else {
+    ctx.body = { products: products.map(mapProduct) };
+  }
 };
