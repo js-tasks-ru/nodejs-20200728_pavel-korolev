2ef94d59e6dbc19b5f6d115d8e72d51d08d30410
Pavel Korolev <p.korolev@.lamoda.ru>
2020-09-08 13:27:16 +0300

    10-websockets/01-chat solved

--- 10-websockets/01-chat/controllers/messages.js
+++ 10-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,24 @@
 const Message = require('../models/Message');
 
+const mapMessage = message => ({
+  date: message.date,
+  id: message._id,
+  text: message.text,
+  user: message.user,
+});
+
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  const { user } = ctx;
+
+  if (!user) {
+    ctx.throw(401, 'Пользователь не залогинен');
+  }
+
+  const messages = await Message.find({ chat: user._id });
+
+  if (!messages) {
+    ctx.body = { messages: [] };
+  }
+
+  ctx.body = { messages: messages.slice(-20).map(mapMessage) };
 };
